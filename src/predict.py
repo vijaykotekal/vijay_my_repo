75b3faec128596eea6024061fcd7bd5d27414163
vijaykotekal <117084979+vijaykotekal@users.noreply.github.com>
2025-07-09 11:30:41 +0530

    Add files via upload

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import joblib
 
+# Load model
+model = joblib.load('model/model.pkl')
+
+# Sample patient
+sample = pd.DataFrame({
+    'age': [54],
+    'sex': [1],
+    'cp': [0],
+    'trestbps': [130],
+    'chol': [250],
+    'fbs': [0],
+    'restecg': [1],
+    'thalach': [150],
+    'exang': [0],
+    'oldpeak': [1.2],
+    'slope': [1],
+    'ca': [0],
+    'thal': [2]
+})
+
+# Predict
+prediction = model.predict(sample)[0]
+risk = "At Risk" if prediction == 1 else " Low Risk"
+print(f" Prediction: {prediction} ({risk})")
